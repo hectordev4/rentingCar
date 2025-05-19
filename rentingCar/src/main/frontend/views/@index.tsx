0ebbcdd8a580c38ef8a7750bf3e75664b5e3ba06
Hectordev4 <hector.valverde.cana@gmail.com>
2025-05-19 13:54:09 +0200

    feat: update home view layout and logo size

--- rentingCar/src/main/frontend/views/@index.tsx
+++ rentingCar/src/main/frontend/views/@index.tsx
@@ -6,11 +6,15 @@ export const config: ViewConfig = { menu: { order: 0, icon: 'line-awesome/svg/ho
 export default function HomeView() {
     console.log(banner);
   return (
+    <>
     <div className="flex flex-col h-full items-center justify-center p-l text-center box-border">
       <img style={{ width: '800px' }} src="icons/Logo.svg" />
-      <br />
-      <h2>This place intentionally left empty</h2>
-      <p>It’s a place where you can grow your own UI 🤗</p>
+      <div className="flex flex-row items-center justify-between gap-xl text-center mt-xl">
+        <button> Button1</button>
+        <button> Button2</button>
+        <button> Button3</button>
+      </div>
     </div>
+    </>
   );
 }
